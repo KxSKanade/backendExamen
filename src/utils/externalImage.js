c4d2644f6c2db8458c6b6ed272bbf0ad2ed06612
kxskanade <terry.lazaro@tecsup.edu.pe>
2025-06-02 16:46:28 -0500

    b

--- src/utils/externalImage.js
+++ src/utils/externalImage.js
@@ -4,10 +4,10 @@ async function fetchRandomImageUrl() {
   try {
     // HEAD sobre la URL externa para obtener el encabezado “location”
     const response = await axios.head(process.env.IMAGE_API_URL);
-    return response.headers.location || 'https://via.placeholder.com/200';
+    return response.headers.location || 'https://picsum.photos/200';
   } catch (error) {
     console.error('Error al obtener imagen externa:', error.message);
-    return 'https://via.placeholder.com/200'; // fallback en caso de error
+    return 'https://picsum.photos/200'; // fallback en caso de error
   }
 }
 
